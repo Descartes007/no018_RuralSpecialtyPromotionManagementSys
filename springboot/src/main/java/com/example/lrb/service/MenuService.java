310dd32b6a852bcf72f5624a36e9df39dd728b70
Descartes007 <162323805+Descartes007@users.noreply.github.com>
2024-06-21 16:49:13 +0800

    基于微信小程序+SpringBoot的乡村特产推广管理系统

--- springboot/src/main/java/com/example/lrb/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.example.lrb.service;
-
-import com.example.lrb.pojo.Menu;
-
-import java.util.List;
-
-public interface MenuService {
-	public List<Menu> getMenusByRoleId(Integer role_id);
-}
